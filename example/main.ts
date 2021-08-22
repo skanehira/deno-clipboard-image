53983b806ceac93c4fd46b43dce370582fd8f8da
skanehira <sho19921005@gmail.com>
2021-08-22 19:16:02 +0900

    close resource

--- example/main.ts
+++ example/main.ts
@@ -7,6 +7,7 @@ import { copy } from "https://deno.land/std@0.105.0/io/mod.ts";
 (async function () {
   const image = await Deno.open("test.png");
   await write(image);
+  image.close();
 
   const src = await read();
   const out = await Deno.create("image.png");
